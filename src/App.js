16cb6d369182c91d0926bbd9de753c5e7f0470c7
jdhokey <jdhokey@deloitte.com>
2025-06-16 13:45:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useMsal } from "@azure/msal-react"; // required for handling redirect
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { useMsal } from "@azure/msal-react";
 import AuthScreen from "./components/AuthScreen";
 import ViewProjects from "./components/ViewProjects";
 import AddProjects from "./components/AddProject";
 
-function App() {
+const AppWrapper = () => {
   const { instance } = useMsal();
+  const navigate = useNavigate();
 
   useEffect(() => {
     instance.handleRedirectPromise().then((response) => {
       if (response !== null) {
         instance.setActiveAccount(response.account);
+        // 🎯 After login success, redirect to /projects
+        navigate('/projects');
+      } else {
+        const account = instance.getAllAccounts()[0];
+        if (account) {
+          instance.setActiveAccount(account);
+          // 🎯 Already logged in? Go to /projects
+          navigate('/projects');
+        }
       }
     });
-  }, [instance]);
+  }, [instance, navigate]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthScreen />} />
-        <Route path="/projects" element={<ViewProjects />} />
-        <Route path="/createprojects" element={<AddProjects />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<AuthScreen />} />
+      <Route path="/projects" element={<ViewProjects />} />
+      <Route path="/createprojects" element={<AddProjects />} />
+    </Routes>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <AppWrapper />
+  </Router>
+);
 
 export default App;
