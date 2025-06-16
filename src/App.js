655223f3d804b34deb2d4792005ffabd25b7e695
jdhokey <jdhokey@deloitte.com>
2025-06-16 13:21:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useMsal } from "@azure/msal-react"; // required for handling redirect
 import AuthScreen from "./components/AuthScreen";
 import ViewProjects from "./components/ViewProjects";
 import AddProjects from "./components/AddProject";
 
 function App() {
+  const { instance } = useMsal();
+
+  useEffect(() => {
+    instance.handleRedirectPromise().then((response) => {
+      if (response !== null) {
+        instance.setActiveAccount(response.account);
+      }
+    });
+  }, [instance]);
+
   return (
     <Router>
       <Routes>
